48dc9da001b2c41807431ac1927866a34c5c1def
Zephyrs <ZephyrsPer@gmail.com>
2024-06-04 21:54:55 +0800

    整理了一下目录结构

--- nest.js/demo/src/demo/demo.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get, Param } from '@nestjs/common';
-@Controller('demo')
-export class DemoController {
-  @Get(':id')
-  findOne(@Param('id') id): string {
-    return `This action returns a #${id} cat`;
-  }
-}
